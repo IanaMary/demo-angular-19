60f6232352fb226208dd9fa0ba420b81c181365d
IanaMary <ianacosta1303@gmail.com>
2025-03-25 16:49:06 -0300

    atualizando.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'angular-api-demo' title`, () => {
+  it(`should have the 'defafio-am53' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-api-demo');
+    expect(app.title).toEqual('defafio-am53');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-api-demo');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, defafio-am53');
   });
 });
